63c3d40557d72762b118dfe6b1d4dd1ac670bfdd
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-07-02 02:43:47 +0300

    add range-slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import noUiSlider from 'nouislider';
+import wNumb from './js/wNumb'
 import "./styles/index.scss";
 
 
+
+let slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [5000, 10000],
+    step: 10,
+    connect: true,
+    margin: 30,
+    range: {
+        'min': 0,
+        'max': 15700
+    },
+    format: wNumb({
+        decimals: 0,
+        thousand: ' ',
+        suffix: '₽'
+    })
+});
+
+const marginMin = document.getElementById('slider-margin-value-min'),
+    marginMax = document.getElementById('slider-margin-value-max');
+
+slider.noUiSlider.on('update', function (values, handle) {
+    if (handle) {
+        marginMax.innerHTML = values[handle];
+    } else {
+        marginMin.innerHTML = values[handle];
+    }
+});
