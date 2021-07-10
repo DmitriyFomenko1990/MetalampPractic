59774ec81eb2dfc83dfa6b56cfdddd0d7e6c8075
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-07-10 18:17:44 +0300

    add dropdown jquery logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import noUiSlider from 'nouislider';
-import wNumb from './js/wNumb'
+import wNumb from './js/wNumb';
 import "./styles/index.scss";
 import $ from "jquery";
 
@@ -24,7 +24,7 @@ noUiSlider.create(slider, {
 });
 
 const marginMin = document.getElementById('slider-margin-value-min'),
-    marginMax = document.getElementById('slider-margin-value-max');
+      marginMax = document.getElementById('slider-margin-value-max');
 
 slider.noUiSlider.on('update', function (values, handle) {
     if (handle) {
@@ -35,36 +35,95 @@ slider.noUiSlider.on('update', function (values, handle) {
 });
 
 $(document).ready(function(){
-    const isLike = $(".like-js");
-    const activeClass = 'likes-button_active';
-    isLike.click(function(e){
-        if ($(this).hasClass(activeClass)){
-            $(this).text(function (i, b){
-                return --b
-            })
-        } else {
-            $(this).text(function (i, b){
-                return ++b
+    /* guest input dropdown */
+    const guestDropdownButton = $(".guests-dropdown-button-js");
+    const guestInput = $(".input-guests-js");
+    const guestDropdown = $(".guests-dropdown-js");
+    const guestClearBtn = $(".quest-clean-js");
+    const allCounters = $(".guest-counter-js");
+
+    guestDropdownButton.click(function (){
+        guestDropdown.toggleClass("guests-input__dropdown-wrapper_active");
+        guestInput.toggleClass("guests-input__input_active");
+    });
+    /* guest more button */
+    const moreBtn =$(".more-js");
+    moreBtn.click(function (){
+        const lessBtn = $(this).siblings(".less-js");
+        const counter = $(this).prev();
+        if (counter.text() === "0") lessBtn.addClass("guests-input__button_active")
+        counter.text(function (i, count){
+            return ++count
+        });
+        changeInputValue();
+    });
+    /* guest less button */
+    const lessBtn =$(".less-js")
+    lessBtn.click(function (){
+        const counter = $(this).next();
+        if (+counter.text() > 0) {
+            counter.text(function (i, count) {
+                return --count
             })
         }
-        $(this).toggleClass(activeClass)
+        if (counter.text() === "0") $(this).removeClass("guests-input__button_active");
+        changeInputValue();
+    });
+    /* quest input validator */
+    function changeInputValue() {
+        let count = 0
+        allCounters.text(function (z, x){
+            count = count + +x
+        })
+        if ( count === 1) {
+            guestClearBtn.addClass("guests-input__clean_active");
+            guestInput.val(function (){
+                return  "1 гость";
+            })
+        } else
+        if (count > 1 && count < 5) {
+            guestInput.val(function (){
+                return `${count} гостя`;
+            })
+        } else
+        if (count >= 5 ) {
+            guestInput.val(function (){
+                return `${count} гостей`;
+            })
+        } else {
+            guestClearBtn.removeClass("guests-input__clean_active")
+            guestInput.val(function (){
+                return 'Сколько гостей';
+            })
+        }
+    }
+    /* clear form */
+    guestClearBtn.click(function (){
+        guestInput.val(function (){
+            return "Сколько гостей";
+        });
+        lessBtn.removeClass("guests-input__button_active");
+        guestClearBtn.removeClass("guests-input__clean_active");
+        allCounters.text(function (){
+            return "0"
+        });
     });
 });
-
-
-/*
-const isLike = document.querySelectorAll('.like-js')
-
-isLike?.forEach(block => likeToggle(block) )
-
-function likeToggle(block){
-    block.addEventListener("click", () => {
-        block.classList.contains("likes-button_active")
-            ? --block.innerHTML
-            : ++block.innerHTML
-        block.classList.toggle("likes-button_active");
+    /* like button */
+    const isLike = $(".like-js");
+    const activeClass = "likes-button_active";
+    isLike.click(function(){
+        if ($(this).hasClass(activeClass)){
+            $(this).text(function (i, count){
+                return --count
+            })
+        } else {
+            $(this).text(function (i, count){
+                return ++count
+            })
+        }
+        $(this).toggleClass(activeClass);
     });
-}
-*/
+
 
 
