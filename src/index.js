de15ff06de6eca16fac73657e27d9f4865cf68d6
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-07-10 22:46:03 +0300

    add searching card

--- src/index.js
+++ src/index.js
@@ -3,33 +3,33 @@ import wNumb from './js/wNumb';
 import "./styles/index.scss";
 import $ from "jquery";
 
-let slider = document.getElementById('slider');
-noUiSlider.create(slider, {
-    start: [5000, 10000],
-    step: 10,
-    connect: true,
-    margin: 30,
-    range: {
-        'min': 0,
-        'max': 15700
-    },
-    format: wNumb({
-        decimals: 0,
-        thousand: ' ',
-        suffix: '₽'
-    })
-});
-
-const marginMin = document.getElementById('slider-margin-value-min'),
-      marginMax = document.getElementById('slider-margin-value-max');
-
-slider.noUiSlider.on('update', function (values, handle) {
-    if (handle) {
-        marginMax.innerHTML = values[handle];
-    } else {
-        marginMin.innerHTML = values[handle];
-    }
-});
+// let slider = document.getElementById('slider');
+// noUiSlider.create(slider, {
+//     start: [5000, 10000],
+//     step: 10,
+//     connect: true,
+//     margin: 30,
+//     range: {
+//         'min': 0,
+//         'max': 15700
+//     },
+//     format: wNumb({
+//         decimals: 0,
+//         thousand: ' ',
+//         suffix: '₽'
+//     })
+// });
+//
+// const marginMin = document.getElementById('slider-margin-value-min'),
+//       marginMax = document.getElementById('slider-margin-value-max');
+//
+// slider.noUiSlider.on('update', function (values, handle) {
+//     if (handle) {
+//         marginMax.innerHTML = values[handle];
+//     } else {
+//         marginMin.innerHTML = values[handle];
+//     }
+// });
 
 $(document).ready(function(){
     /* guest input dropdown */
