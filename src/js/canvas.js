8cce78e6493d74eab458beb35cf9dc7611244d60
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-07-21 19:19:58 +0300

    refactor canvas variable add adaptive on rooms-details page

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -4,36 +4,36 @@ const radians = function(deg) {
 const canvas = document.querySelector('.content-room__canvas');
 console.log(canvas)
 if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
+    const grafic = canvas.getContext('2d');
 
-    ctx.beginPath();
-    const gradientRthDwn = ctx.createLinearGradient(120,60, 60,120);
+    grafic.beginPath();
+    const gradientRthDwn = grafic.createLinearGradient(120,60, 60,120);
     gradientRthDwn.addColorStop(0.9, 'rgba(102, 210, 234, 1)');
     gradientRthDwn.addColorStop(0.1, 'rgba(111, 207, 151, 1)');
 
-    ctx.lineWidth = 4
-    ctx.strokeStyle = gradientRthDwn
-    ctx.arc(60, 60, 56, radians(1), radians(89) ); // Outer circle
-    ctx.stroke();
+    grafic.lineWidth = 4
+    grafic.strokeStyle = gradientRthDwn
+    grafic.arc(60, 60, 56, radians(1), radians(89) ); // Outer circle
+    grafic.stroke();
 
-    ctx.beginPath();
-    const gradientLeft = ctx.createLinearGradient(60,120, 60,0);
+    grafic.beginPath();
+    const gradientLeft = grafic.createLinearGradient(60,120, 60,0);
     gradientLeft.addColorStop(1, 'rgba(255, 227, 156, 1)');
     gradientLeft.addColorStop(0, 'rgba(255, 186, 156, 1)');
 
-    ctx.lineWidth = 4
-    ctx.strokeStyle = gradientLeft
-    ctx.arc(60, 60, 56, radians(91), radians(269) ); // Outer circle
-    ctx.stroke();
+    grafic.lineWidth = 4
+    grafic.strokeStyle = gradientLeft
+    grafic.arc(60, 60, 56, radians(91), radians(269) ); // Outer circle
+    grafic.stroke();
 
-    ctx.beginPath();
-    const gradientRthTop = ctx.createLinearGradient(60,0, 120,60);
+    grafic.beginPath();
+    const gradientRthTop = grafic.createLinearGradient(60,0, 120,60);
     gradientRthTop.addColorStop(0.5, 'rgba(188, 156, 255, 1)');
     gradientRthTop.addColorStop(0.99, 'rgba(139, 164, 249, 1)');
 
-    ctx.lineWidth = 4
-    ctx.strokeStyle = gradientRthTop
-    ctx.arc(60, 60, 56, radians(271), radians(359) ); // Outer circle
-    ctx.stroke();
+    grafic.lineWidth = 4
+    grafic.strokeStyle = gradientRthTop
+    grafic.arc(60, 60, 56, radians(271), radians(359) ); // Outer circle
+    grafic.stroke();
 
 }
